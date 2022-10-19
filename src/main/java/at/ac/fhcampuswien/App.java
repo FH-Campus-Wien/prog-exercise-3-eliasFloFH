32ef90c87d9deddbf1d793964630f98bedd52256
eliasFloFH <elias.damisch@stud.fh-campuswien.ac.at>
2022-10-19 15:58:10 +0200

    first commit

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -7,10 +7,50 @@ public class App {
 
     // Implement all methods as public static
 
+    public static void oneMonthCalender(int numberDays, int startingDay) {
+
+        int countDay = 1;
+        StringBuilder initialSpace = new StringBuilder();
+
+        for (int x = 0; x < startingDay - 1; x++) {
+            initialSpace.append("   ");
+        }
+
+        System.out.print(initialSpace);
+
+        for (int i = 1; i <= numberDays; i++) {
+
+            if (i < 10) {
+                System.out.printf("% 2d ", i);
+            } else {
+                System.out.printf("%2d ", i);
+            }
+
+            if (countDay % 7 == 0 && startingDay == 1) {
+                System.out.println();
+            }
+
+
+
+            countDay++;
+
+
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
         // test your method implementations here
         // make method calls
         // print their results
         // etc.
+
+
+    oneMonthCalender(31, 1);
+
+
+
     }
 }
